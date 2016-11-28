9f470f6c83f624cf0cea86d0688bcff990b360ac
Soenke Ruempler <soenke@ruempler.eu>
2016-11-28 22:31:11 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to the serverless-boilerplate based on create-react-app</h2>
         </div>
         <pre className="App-intro">{JSON.stringify(this.state.data, null, 2)}</pre>
       </div>
